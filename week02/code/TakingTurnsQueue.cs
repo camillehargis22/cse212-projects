a1abea25f320c7a88fe89ceacd705aee87a8d78b
Camille Hargis <camillehargis@studentbody.byu.edu>
2025-09-13 20:20:21 -0600

    w02 taking turns queue

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 /// <summary>
 /// This queue is circular.  When people are added via AddPerson, then they are added to the 
 /// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
@@ -43,6 +45,14 @@ public class TakingTurnsQueue
             if (person.Turns > 1)
             {
                 person.Turns -= 1;
+                _people.Enqueue(person);
+            }
+            else if (person.Turns == 1)
+            {
+                return person;
+            }
+            else
+            {
                 _people.Enqueue(person);
             }
 
