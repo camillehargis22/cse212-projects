c0f8471e38ab78ec69acb5f089e41f98c593f5d5
Camille Hargis <camillehargis@studentbody.byu.edu>
2025-09-30 00:58:51 -0600

    w04 code linked lists practice

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -33,6 +33,21 @@ public class LinkedList : IEnumerable<int>
     public void InsertTail(int value)
     {
         // TODO Problem 1
+        // create new node
+        Node newNode = new(value);
+
+        // If the list is empty, then point head and tail to new node
+        if (_tail is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Prev = _tail;
+            _tail.Next = newNode;
+            _tail = newNode;
+        }
     }
 
 
@@ -65,6 +80,16 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+        if (_head == _tail)
+        {
+            _head = null;
+            _tail = null;
+        }
+        else if (_tail is not null)
+        {
+            _tail.Prev.Next = null;
+            _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
@@ -109,6 +134,38 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
+        var current = _head;
+
+        if (_head == null)
+        {
+            return;
+        }
+        if (_head.Data == value)
+        {
+            RemoveHead();
+            return;
+        }
+
+        while (current != null)
+        {
+            if (current.Data == value)
+            {
+                if (current == _tail)
+                {
+                    RemoveTail();
+                }
+                else
+                {
+                    current.Next.Prev = current.Prev;
+                    current.Prev.Next = current.Next;
+                }
+                break;
+            }
+            else
+            {
+                current = current.Next;
+            }
+        }
     }
 
     /// <summary>
@@ -117,6 +174,15 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data == oldValue)
+            {
+                current.Data = newValue;
+            }
+            current = current.Next;
+        }
     }
 
     /// <summary>
@@ -147,7 +213,13 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        // yield return 0; // replace this line with the correct yield return statement(s)
+        var curr = _tail;
+        while (curr is not null)
+        {
+            yield return curr.Data;
+            curr = curr.Prev;
+        }
     }
 
     public override string ToString()
